b70072318c3db8c9e451b00a196271adb1e540b6
Dona Ghosh <donaghosh3110@gmail.com>
2025-06-08 19:49:21 +0530

    Update new.cpp

--- competitive-programming/Program-to-Copy-One-File-into-Another-File/new.cpp
+++ competitive-programming/Program-to-Copy-One-File-into-Another-File/new.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define dona DONA
+#define ll long long int // 64-bit integer -> 2^63 ~ 10^18 
 
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	#ifndef dona
+		// int n = INT_MAX;  //2^31 - 1
+		ll n = 1e18; //= 10^18, 2e18 = (2*10)^18
+		cin >> n;
+		cout << n;
+	#endif
+
+	return 0;
+}
