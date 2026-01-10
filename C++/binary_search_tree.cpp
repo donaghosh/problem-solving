f54be6a63bac9eb4ef0486bad4b0d774984761de
Dona Ghosh <donaghosh3110@gmail.com>
2026-01-10 21:00:36 +0530

    Refactor binary search tree and add min/max functions
    Refactor binary search tree implementation by renaming functions and improving structure. Added FindMin and FindMax functions to retrieve minimum and maximum values.

--- C++/binary_search_tree.cpp
+++ C++/binary_search_tree.cpp
@@ -1,54 +1,67 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 struct Node{
-    int data;
     Node* left;
+    int data;
     Node* right;
 };
-
-Node* MakeNewNode(int data) {
-    Node* newNode = new Node();
-    newNode->data = data;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
+Node* CreateNewNode(int data){
+    Node* node = new Node();
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
 }
-
-Node* Insert(Node* head, int data){
-    if (head==NULL){
-        head = MakeNewNode(data);
-    }
-    else
-    {
-        if(data > head->data){
-            head->right = Insert(head->right, data);
-        }
-        else
-        {
-            head->left = Insert(head->left, data);
+Node* Insert(Node* root, int data){
+    if(root == NULL){
+        root = CreateNewNode(data);
+    }else{
+        if(data>root->data){
+            root->right = Insert(root->right, data);
+        }else{
+            root->left = Insert(root->left, data);
         }
     }
-    return head;
+    return root;
 }
-
 bool Search(Node* root, int data){
-    if (root == NULL){
+    if(root == NULL){
         return false;
-    }else if(data == root->data){
+    }else if(root->data == data){
         return true;
-    }else if(data > root->data){
+    }else if(data>root->data){
         return Search(root->right, data);
     }else{
         return Search(root->left, data);
     }
+    return false;
 }
-
+int FindMin(Node* root){
+    if(root==NULL){
+        return -1;
+    }
+    while(root->left!=NULL){
+        root=root->left;
+    }
+    return root->data;
+}
+int FindMax(Node* root){
+    if(root == NULL){
+        return -1;
+    }
+    while(root->right != NULL){
+        root=root->right;
+    }
+    return root->data;
+}
 int main() {
     Node* root = NULL;
-    root = Insert(root, 4);
-    root = Insert(root, 3);
     root = Insert(root, 5);
-    bool IsPresent = Search(root, 8);
-    std::cout<<IsPresent;
+    root = Insert(root, 3);
+    root = Insert(root, 4);
+    root = Insert(root, 2);
+    root = Insert(root, 6);
+    std::cout << Search(root,4)<<std::endl;
+    std::cout << FindMin(root)<<std::endl;
+    std::cout << FindMax(root)<<std::endl;
     return 0;
 }
