4771ddb6989b71ecf4ba52b28f9a85c0ccaa8f77
Dona Ghosh <donaghosh3110@gmail.com>
2026-01-11 19:10:39 +0530

    Implement tree height and traversal functions
    Added functions to calculate height and perform BFS, PreOrder, InOrder, and PostOrder traversals.

--- C++/binary_search_tree.cpp
+++ C++/binary_search_tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 struct Node{
     Node* left;
     int data;
@@ -53,6 +54,46 @@ int FindMax(Node* root){
     }
     return root->data;
 }
+int FindHeight(Node* node) {
+    if(node == NULL){
+        return -1;
+    }
+    int left_height = FindHeight(node->left);
+    int right_height = FindHeight(node->right);
+    return std::max(left_height, right_height) + 1;
+}
+void BFS(Node* root){
+    if (root == NULL){
+        return;
+    }
+    std::queue<Node*> Q;
+    Q.push(root);
+    while(!Q.empty()){
+        Node* current = Q.front();
+        std::cout<<current->data<<std::endl;
+        if(current->left!=NULL)Q.push(current->left);
+        if(current->right!=NULL)Q.push(current->right);
+        Q.pop();
+    }
+}
+void PreOrder(Node* root){
+    if(root==NULL) return;
+    std::cout<<root->data;
+    PreOrder(root->left);
+    PreOrder(root->right);
+}
+void PostOrder(Node* root){
+    if(root==NULL) return;
+    PostOrder(root->left);
+    PostOrder(root->right);
+    std::cout<<root->data;
+}
+void InOrder(Node* root){
+    if(root==NULL) return;
+    InOrder(root->left);
+    std::cout<<root->data;
+    InOrder(root->right);
+}
 int main() {
     Node* root = NULL;
     root = Insert(root, 5);
@@ -63,5 +104,10 @@ int main() {
     std::cout << Search(root,4)<<std::endl;
     std::cout << FindMin(root)<<std::endl;
     std::cout << FindMax(root)<<std::endl;
+    std::cout << FindHeight(root)<<std::endl;
+    BFS(root);
+    PreOrder(root);
+    InOrder(root);
+    PostOrder(root);
     return 0;
 }
