3f129fdb297959582403afeb3d4d5eef1de2311b
Dona Ghosh <donaghosh3110@gmail.com>
2026-04-25 14:09:51 +0530

    Enhance comments for car toll calculator
    Added detailed comments explaining the car toll calculator functionality, including input and output examples.

--- C++/car_toll_calculator.cpp
+++ C++/car_toll_calculator.cpp
@@ -1,3 +1,41 @@
+// Car Toll Collector
+// Simulate car movement on grid, tracking direction and toll on axis-to-quadrant transitions.
+// For a car that starts-at (0, 0) facing +X (right) on an infinite grid. The following commands are allowed:
+// TL → turn left (90")
+// TR → turn right (90")
+// Fn → move forward n steps
+// Toll criteria:
+// Quadrant entry costs when moving between quadrants/ from axisto quadrants:
+// Quadrant 1: (+,+) → $ 1
+// Quadrant 2: (-,+) → $ 2
+// Quadrant 3: (-,-)→ $ 3
+// Quadrant 4: (+,-) → $ 4 No toll for: moving on axis
+// staying within a quadrant during movement • moving from quadrant → axis
+// Re-entering a quadrant from an axis charges again.
+// Input
+// ["command_1", "command_2",.. "command _N"]
+// Output:
+// Toll Amount: total toll amount
+// Position: Final x, final y
+// Example:
+// Input:
+// ["F1", "TL", "F2", "TL", "F4", "TL", "F4", "TL", "F3", "TL", "F5"]
+// Output:
+// Toll Amount: 6
+// Position: 03
+// Explanation:
+// F1 : (0,0) → (1,0) : axis : no toll
+// • TL: new direction: UP
+// • F2: (1,0) → (1,2) : axis → Q1: + $ 1
+// • TL: new direction: LEFT
+// • F4 : (1,2) → (-3,2) : Q1 → axis → Q2: + $ 2
+// • TL: new direction: DOWN
+// F4 : (-3,2) → (-3,-2) : Q2 → axis → Q3 : + $ 3
+// • TL: new direction: RIGHT
+// • F3: (-3,-2) → (0,-2) : Q3 → axis : no toll
+// • TL: new direction: UP
+// • F5 (0,-2) → (0,3) : axis : no toll
+
   #include<iostream>
   #include <vector>
   #include <string>
